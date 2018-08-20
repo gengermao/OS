34d6d927134ef99b0f1955ac4df20fa2711b6a40
gengermao <gengermao@126.com>
2018-08-20 14:42:24 +0800

    删除汇编式写入内存地址的函数，采用指针写法

--- bootpack.c
+++ bootpack.c
@@ -1,11 +1,18 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
+
 
 void HariMain(void)
 {
 	int i;
-	for(i = 0xa0000; i <= 0xaffff; i++){
-		write_mem8(i,i & 0x0f);
+	char *p;
+	
+	p = (char *) 0xa0000;
+	
+	for(i =0; i <= 0xffff; i++){
+		//p = i;//带入地址
+		//*p = i & 0x0f;
+		//*(p + i) = i & 0x0f;
+		p[i] = i & 0x0f;
 	}
 	for(;;){
 		io_hlt();
